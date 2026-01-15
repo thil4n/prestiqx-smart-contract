16319e5e70133905e773464ed9b0363345c65a7c
Thilan Dissanayaka <43875064+Thil4n@users.noreply.github.com>
2026-01-15 18:09:40 +0530

    Update contract

--- contracts/EventManager.sol
+++ contracts/EventManager.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract EventManager {
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract EventManager is ERC721, Ownable {
     struct Event {
         uint256 id;
         address organizer;
@@ -19,10 +22,19 @@ contract EventManager {
     }
 
     uint256 public nextEventId;
+    uint256 public nextTicketId;
+
     mapping(uint256 => Event) public events;
 
     uint256[] public allEvents;
     mapping(address => uint256[]) public organizerEvents;
+    mapping(address => uint256[]) public eventTickets;
+
+    mapping(uint256 => uint256) public ticketToEvent;
+
+    constructor() ERC721("EventTicket", "ETICKET") Ownable(msg.sender) {
+        // Initialization if needed
+    }
 
     function createEvent(
         string memory title,
@@ -67,5 +79,20 @@ contract EventManager {
 
     function getEvent(uint256 eventId) external view returns (Event memory) {
         return events[eventId];
+    }
+
+    function buyTicket(uint256 eventId) external payable {
+        Event storage e = events[eventId];
+        require(e.id == eventId, "Event does not exist");
+        require(!e.ended, "Event ended");
+        require(e.ticketsSold < e.totalTickets, "Sold out");
+        require(msg.value >= e.ticketPrice, "Not enough ETH sent");
+
+        // Mint NFT ticket
+        uint256 ticketId = nextTicketId++;
+        _mint(msg.sender, ticketId);
+        ticketToEvent[ticketId] = eventId;
+
+        e.ticketsSold++;
     }
 }
